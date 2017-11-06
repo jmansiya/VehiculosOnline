505113854b0911027b7ab42934ee750057c92660
Jose Mansilla <jmansiya@users.noreply.github.com>
2017-11-06 01:09:07 +0100

    SE añade conf. para utilizar con eureka, y con spring boot 1.5 para temas de seguridad.

--- Vehiculos/src/main/java/com/jmansilla/vehiculos/Application.java
+++ Vehiculos/src/main/java/com/jmansilla/vehiculos/Application.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
